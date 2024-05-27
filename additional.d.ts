cccd49a4a61f8ceb8fde507858dd0ab719def603
James Watt <james.watt529@outlook.com>
2024-05-27 18:15:36 +0100

    Remove eslint ignore comments, add interface for envs

--- additional.d.ts
+++ additional.d.ts
@@ -7,3 +7,8 @@ interface OrcaLayer {
   img: HTMLImageElement,
   id: number
 }
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_UMAMI_WEBSITE_ID: string
+  readonly VITE_UMAMI_WEBSITE_URL: string
+}
