d44aa155cf7c871c83cfb6ac6c5dc29513347bb5
James Watt <james.watt529@outlook.com>
2024-05-27 15:44:20 +0100

    Add orcaActions file

---
+++ src/orcaActions.ts
@@ -0,0 +1,31 @@
+import { ORCA_IMAGE_URLS } from "./constants";
+
+export const loadOrcaLayers = (): Promise<OrcaLayer[]> => {
+  const orcaImages = ORCA_IMAGE_URLS.map(() => new Image());
+
+  const loadingPromises:  Promise<OrcaLayer>[] = orcaImages.map((image, index) => {
+    return new Promise<OrcaLayer>(
+      (res, rej) => {
+        image.onload = () => res({ img: image, id: index });
+        image.onerror = () => rej({ img: image, id: index });
+      }
+    )
+  })
+
+  orcaImages.forEach((image, index) => {
+    image.src = ORCA_IMAGE_URLS[index];
+  });
+
+  return Promise.all(loadingPromises);
+}
+
+// assuming 1.png is nose
+export const sortOrcaLayersTailFirst = (layers: OrcaLayer[]) => {
+  layers.sort((a, b) => {
+    if (a.id > b.id) {
+      return -1
+    } else {
+      return 1;
+    }
+  });
+}
