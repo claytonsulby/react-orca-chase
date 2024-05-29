e1d2d2dc2e7b5dd5b187baba22cb88c2a4e89963
James Watt <james.watt529@outlook.com>
2024-05-29 18:54:14 +0100

    Add james-watt-calling-card with types

--- additional.d.ts
+++ additional.d.ts
@@ -1,14 +1,27 @@
-interface Point {
-  x: number;
-  y: number;
-}
+import { JamesWattCallingCard } from "james-watt-calling-card/src/james-watt-calling-card.js";
 
-interface OrcaLayer {
-  img: HTMLImageElement;
-  id: number;
-}
+// Web component type implementation from https://coryrylan.com/blog/how-to-use-web-components-in-preact-and-typescript
+type CustomElement<T> = Partial<T & { children: JSX.Element; class: string }>;
 
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
-  readonly VITE_UMAMI_WEBSITE_ID: string;
-  readonly VITE_UMAMI_WEBSITE_URL: string;
+declare global {
+  interface Point {
+    x: number;
+    y: number;
+  }
+
+  interface OrcaLayer {
+    img: HTMLImageElement;
+    id: number;
+  }
+
+  interface ImportMetaEnv extends Readonly<Record<string, string>> {
+    readonly VITE_UMAMI_WEBSITE_ID: string;
+    readonly VITE_UMAMI_WEBSITE_URL: string;
+  }
+
+  namespace JSX {
+    interface IntrinsicElements {
+      "james-watt-calling-card": CustomElement<typeof JamesWattCallingCard>;
+    }
+  }
 }
